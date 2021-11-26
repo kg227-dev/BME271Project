1450093e6ae236074d9adcea39547462d88dad2c
Jake Spruance <jacob.spruance@duke.edu>
2021-11-26 15:41:15 -0500

    Added test sound with harmonics and reworked envelope with interp1()

--- Test_Code.m
+++ Test_Code.m
@@ -4,32 +4,64 @@
 % works again without changing anything on second run?
 % Adjustable values to be convolved with sounds
 
-t = 0:0.001:3.2;
-td = 0.001:0.001:d/1000;
-tr = 0.001:0.001:r/1000;
-dt = 0.001;
-a = 200;
-d = 0;
-s = 1;
-r = 200;
+[trumpet,fs_trumpet] = audioread('/Users/Jake/BME271/Final Project/BME271Project/trumpet-C4.wav');
+[violin,fs_violin] = audioread('/Users/Jake/BME271/Final Project/BME271Project/violin-C4.wav');
+[piano,fs_piano] = audioread('/Users/Jake/BME271/Final Project/BME271Project/piano-C4.wav');
+[flute,fs_flute] = audioread('/Users/Jake/BME271/Final Project/BME271Project/flute-C4.wav');
 
-env = ones(1, 3201);
+% a + d + r + s < length of sound, ms
+% samp < 1
+% Params to adjust 
+a = 100;
+d = 100;
+s = 200;
+r = 2000;
+samp = 0.8;
 
-for i = 1:length(t)
-    if i <= a+1
-        env(i) = 1/a * t(i) * 1000;
-    elseif i < a + d + 1
-        env(i) = 1 + td(i-a) * ((s - 1)/d)*1000;
-    elseif i < a + d + 201
-        env(i) = s;
-    elseif i < a + d + r + 201
-        env(i) = s - tr(i-a-d-200) * (s/r)*1000;
-    else
-        env(i) = 0;
-    end
-end
+fs = 11025;
+dt = 1/fs;
 
+t = 0:dt:3.2;
+td = dt:dt:d/1000;
+tr = dt:dt:r/1000;
+
+% Make the envelope
+% Plot xq, vq to visualize this
+x = [0 a/1000 (a+d)/1000 (a+d+s)/1000 (a+d+r+s)/1000];
+y = [0 1 samp samp 0];
+xq = 0:dt:3.2;
+vq = interp1(x, y, xq);
+
+ttrumpet = [0:1/fs_trumpet:length(trumpet)/fs_trumpet];
+tviolin = [0:1/fs_violin:length(violin)/fs_violin];
+tpiano = [0:1/fs_piano:length(piano)/fs_piano];
+tflute = [0:1/fs_flute:length(flute)/fs_flute];
+
+% Amplitudes of harmonics
+h1 = 0.01;
+h2 = 0.02;
+h3 = 0.04;
+h4 = 0.05;
+h5 = 0.03;
+h6 = 0.05;
+h7 = 0.03;
+h8 = 0;
+
+% f0 for C4
+f0 = 261.6;
+% Test sound
+test = h1 * cos(2*pi*f0*t) + h2 * cos(4*pi*f0*6) + h3 * cos(6*pi*f0*t) + ...
+    h4 * cos(8*pi*f0*t) + h5 * cos(10*pi*f0*t) + h6 * cos(12*pi*f0*t) + ...
+    h7 * cos(14*pi*f0*t) + h8 * cos(16*pi*f0*t);
+
+% Play test sound with envelope
+soundsc(test .* vq, fs)
+
+%% Plot Envelope
 figure(1)
-plot(t, env)
+plot(xq, vq)
 ylim([0 1.2])
 xlim([0 3.2])
+
+figure(2)
+plot(piano)
