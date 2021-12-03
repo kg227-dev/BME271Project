daacbc484b749d61b2dd4a3fbb79b7afecae4077
Jake Spruance <jacob.spruance@duke.edu>
2021-12-03 17:20:06 -0500

    instruments clipped for envelope stuff

--- Test_Code.m
+++ Test_Code.m
@@ -4,14 +4,14 @@
 % works again without changing anything on second run?
 % Adjustable values to be convolved with sounds
 
-%[trumpet,fs_trumpet] = audioread('/Users/Jake/BME271/Final Project/BME271Project/trumpet-C4.wav');
-[trumpet,fs_trumpet] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/trumpet-C4.wav');
-%[violin,fs_violin] = audioread('/Users/Jake/BME271/Final Project/BME271Project/violin-C4.wav');
-[violin,fs_violin] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/violin-C4.wav');
-%piano,fs_piano] = audioread('/Users/Jake/BME271/Final Project/BME271Project/piano-C4.wav');
-[piano,fs_piano] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/piano-C4.wav');
-%[flute,fs_flute] = audioread('/Users/Jake/BME271/Final Project/BME271Project/flute-C4.wav');
-[flute,fs_flute] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/flute-C4.wav');
+[trumpet,fs_trumpet] = audioread('/Users/Jake/BME271/Final Project/BME271Project/trumpet-C4.wav');
+%[trumpet,fs_trumpet] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/trumpet-C4.wav');
+[violin,fs_violin] = audioread('/Users/Jake/BME271/Final Project/BME271Project/violin-C4.wav');
+%[violin,fs_violin] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/violin-C4.wav');
+[piano,fs_piano] = audioread('/Users/Jake/BME271/Final Project/BME271Project/piano-C4.wav');
+%[piano,fs_piano] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/piano-C4.wav');
+[flute,fs_flute] = audioread('/Users/Jake/BME271/Final Project/BME271Project/flute-C4.wav');
+%[flute,fs_flute] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/flute-C4.wav');
 
 % a + d + r + s < length of sound, ms
 % samp < 1
@@ -25,7 +25,7 @@ samp = 0.8;
 fs = 11025;
 dt = 1/fs;
 
-t = 0:dt:3.2;
+t = 0:dt:2.6;
 td = dt:dt:d/1000;
 tr = dt:dt:r/1000;
 
@@ -33,9 +33,13 @@ tr = dt:dt:r/1000;
 % Plot xq, vq to visualize this
 x = [0 a/1000 (a+d)/1000 (a+d+s)/1000 (a+d+r+s)/1000];
 y = [0 1 samp samp 0];
-xq = 0:dt:3.2;
+xq = 0:dt:2.6;
 vq = interp1(x, y, xq);
 
+trumpet_clip = transpose(trumpet(1:length(vq)));
+violin_clip = transpose(violin(1:length(vq)));
+piano_clip = transpose(piano(1:length(vq)));
+flute_clip = transpose(flute(1:length(vq)));
 
 %ttrumpet = [0:1/fs_trumpet:length(trumpet)/fs_trumpet];
 ttrumpet = (0:1/fs_trumpet:(length(trumpet)-1)/fs_trumpet);
@@ -55,13 +59,13 @@ tflute = [0:1/fs_flute:(length(flute) - 1)/fs_flute];
 
 % Amplitudes of harmonics
 h1 = 1;
-h2 = 1;
+h2 = 0.3;
 h3 = 1;
-h4 = 1;
-h5 = 1;
+h4 = 0.1;
+h5 = 0.7;
 h6 = 1;
-h7 = 1;
-h8 = 1;
+h7 = 0.01;
+h8 = 0.1;
 
 % f0 for C4
 f0 = 261.6;
@@ -70,15 +74,13 @@ test = h1 * cos(2*pi*f0*t) + h2 * cos(4*pi*f0*t) + h3 * cos(6*pi*f0*t) + ...
     h4 * cos(8*pi*f0*t) + h5 * cos(10*pi*f0*t) + h6 * cos(12*pi*f0*t) + ...
     h7 * cos(14*pi*f0*t) + h8 * cos(16*pi*f0*t);
 
-<<<<<<< HEAD
 test_ft = fftshift(fft(test));
 ft = linspace(-fs/2, fs/2, length(test_ft));
-=======
-
->>>>>>> aa448362a82b8d6e8f002df059f22ec1d1aa2afb
 
 % Play test sound with envelope
+%soundsc(test, fs)
 %soundsc(test .* vq, fs)
+soundsc(piano_clip, fs)
 
 %% Plot Envelope
 subplot(2,2,1)
@@ -87,7 +89,6 @@ subplot(2,2,2)
 plot(xq, vq)
 ylim([0 1.2])
 xlim([0 3.2])
-<<<<<<< HEAD
 
 figure(2)
 plot(transpose(ttrumpet), trumpet)
@@ -95,7 +96,11 @@ plot(transpose(ttrumpet), trumpet)
 figure(3)
 plot(ft, abs(test_ft))
 xlim([0 2200])
-=======
 subplot(2,2,3)
 plot(x,y)
->>>>>>> aa448362a82b8d6e8f002df059f22ec1d1aa2afb
+
+figure(4)
+plot(t, test)
+hold on
+plot(t, test.*vq)
+hold off
