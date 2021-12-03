081fe3dc7a8c601baee2266e4ff2e43d35a9f86b
Kush Gulati <kush.gulati@duke.edu>
2021-12-03 13:44:34 -0500

    dd

--- Test_Code.m
+++ Test_Code.m
@@ -4,10 +4,14 @@
 % works again without changing anything on second run?
 % Adjustable values to be convolved with sounds
 
-[trumpet,fs_trumpet] = audioread('/Users/Jake/BME271/Final Project/BME271Project/trumpet-C4.wav');
-[violin,fs_violin] = audioread('/Users/Jake/BME271/Final Project/BME271Project/violin-C4.wav');
-[piano,fs_piano] = audioread('/Users/Jake/BME271/Final Project/BME271Project/piano-C4.wav');
-[flute,fs_flute] = audioread('/Users/Jake/BME271/Final Project/BME271Project/flute-C4.wav');
+%[trumpet,fs_trumpet] = audioread('/Users/Jake/BME271/Final Project/BME271Project/trumpet-C4.wav');
+[trumpet,fs_trumpet] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/trumpet-C4.wav');
+%[violin,fs_violin] = audioread('/Users/Jake/BME271/Final Project/BME271Project/violin-C4.wav');
+[violin,fs_violin] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/violin-C4.wav');
+%piano,fs_piano] = audioread('/Users/Jake/BME271/Final Project/BME271Project/piano-C4.wav');
+[piano,fs_piano] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/piano-C4.wav');
+%[flute,fs_flute] = audioread('/Users/Jake/BME271/Final Project/BME271Project/flute-C4.wav');
+[flute,fs_flute] = audioread('/Users/kushgulati/Box Sync/Junior Year/BME271/BME271Project/flute-C4.wav');
 
 % a + d + r + s < length of sound, ms
 % samp < 1
@@ -32,10 +36,15 @@ y = [0 1 samp samp 0];
 xq = 0:dt:3.2;
 vq = interp1(x, y, xq);
 
-ttrumpet = [0:1/fs_trumpet:length(trumpet)/fs_trumpet];
-tviolin = [0:1/fs_violin:length(violin)/fs_violin];
-tpiano = [0:1/fs_piano:length(piano)/fs_piano];
-tflute = [0:1/fs_flute:length(flute)/fs_flute];
+%ttrumpet = [0:1/fs_trumpet:length(trumpet)/fs_trumpet];
+ttrumpet = (0:1/fs_trumpet:(length(trumpet)-1)/fs_trumpet);
+%tviolin = [0:1/fs_violin:length(violin)/fs_violin];
+tviolin = (0:1/fs_violin:(length(violin)-1)/fs_violin);
+%tpiano = [0:1/fs_piano:length(piano)/fs_piano];
+tpiano = (0:1/fs_piano:(length(piano)-1)/fs_piano);
+%tflute = [0:1/fs_flute:length(flute)/fs_flute];
+tflute = (0:1/fs_flute:(length(flute)-1)/fs_flute);
+
 
 % Amplitudes of harmonics
 h1 = 0.01;
@@ -58,10 +67,11 @@ test = h1 * cos(2*pi*f0*t) + h2 * cos(4*pi*f0*6) + h3 * cos(6*pi*f0*t) + ...
 soundsc(test .* vq, fs)
 
 %% Plot Envelope
-figure(1)
+subplot(2,2,1)
+plot(tpiano, piano)
+subplot(2,2,2)
 plot(xq, vq)
 ylim([0 1.2])
 xlim([0 3.2])
-
-figure(2)
-plot(piano)
+subplot(2,2,3)
+plot(trumpet.*vq)
